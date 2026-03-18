986024c4d6cf6e137d8d6cdf7761a4947bd10a6a
Andreas Olsson <photoguy.apo@gmail.com>
2026-03-18 15:35:49 +0100

    Added a visualisation

--- main.py
+++ main.py
@@ -7,23 +7,36 @@ import numpy
 import matplotlib.pyplot
 import matplotlib.ticker
 
-rows = []
-dandh = []
-R = 6371000.0 # meters
+# För utritning i plan
+from shapely.geometry import LineString
+import geopandas as gpd
 
+
+# For raw data
+rows = []
+
+# For calcuation
+dandh = []
+
+# Average Earth radius in meters
+R = 6371000.0
+
+# Load a CSV file
 with open("CSV/13141948.CSV", newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         rows.append(row)
 
-# for row in rows[:5]: print(row) # control
+# for row in rows[:5]: print(row) # for control
 
+# Remove trailing chars
 for row in rows[1:]:
     row[4] = row[4][:-1]
     row[5] = row[5][:-1]
 
-# for row in rows[:5]: print(row) # control
+# for row in rows[:5]: print(row) # for control
 
+# Compute length between points
 for index in range(len(rows[:-2])):
     if index == 0:
         dandh.append([0.0, float(rows[index + 1][6])])
@@ -41,21 +54,30 @@ for index in range(len(rows[:-2])):
         b = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
         distance = R * b
-
-        if distance > 0.0: #dont add lengths of zero
+        # Filter out length of zero and append data
+        if distance > 0.0:
             dandh.append([distance, float(rows[index + 1][6])])
 
+# Just print the data
 for row in dandh[:5]:print(row)
 print('----')
 
+# Calculate length from first point on path
 for index in range(1, len(dandh)):
         dandh[index][0] = dandh[index][0] + dandh[index - 1][0]
 
+# Just print the data
 for row in dandh[:5]:print(row)
 
+# Package it for numpy
 distance = numpy.array([value[0] for value in dandh])
 elevation = numpy.array([value[1] for value in dandh])
 
-matplotlib.pyplot.plot(distance, elevation)
-matplotlib.pyplot.show()
+# Visualise the data
+ax1 = matplotlib.pyplot.plot(distance, elevation)
 
+# Draw plane
+coords = [(float(row[5]), float(row[4])) for row in rows[1:]]
+gdf = gpd.GeoDataFrame(geometry=[LineString(coords)], crs="EPSG:4326")
+ax2 = gdf.plot()
+matplotlib.pyplot.show()
